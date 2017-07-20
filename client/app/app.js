cbc05d7a917cc46d0527cc3fefe901e03d646dac
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-07-19 20:28:32 -0500

    added functionality for post

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,7 @@ app.config(['$routeProvider', function ($routeProvider) {
             templateUrl: '../views/home.html'
         }).when('/list', {
             templateUrl: '../views/list.html'
-        }).when('/single', {
+        }).when('/single/:id', {
             templateUrl: '../views/single.html'
         }).when('/add', {
             templateUrl: '../views/add.html'
@@ -15,24 +15,33 @@ app.config(['$routeProvider', function ($routeProvider) {
     }]);
     
 var controlApp = angular.module('controllers', []);
-app.controller('chirpReq', function($scope, $http) {
+controlApp.controller('chirpReq', function($scope, $http, $location, $routeParams) {
     $http.get('/api/chirps')
     .then(function (response) {
-        console.log(response);
       $scope.chirpList = response.data;
-      });
+    });
 
-    $scope.insertData=function(){
+$scope.insertData = function(){
         $http.post('/api/chirps', {'user': $scope.user,'message': $scope.message})
-        .success(function(response){
-        $scope.chirpList = response.data;
-        $scope.user = "";
-        $scope.message = "";
-        })
-        
-        
+            .success(function(response){
+            $scope.chirpList = 
+            console.log("sent post");
+            $scope.user = "";
+            $scope.message = "";
+            $http.get('/api/chirps')
+                .then(function (response) {
+                console.log(response);
+                $scope.chirpList = response.data;
+            });
+        });
     }
+    id = $routeParams.id
+    console.log(id);
+$scope.goToSingle = function(id){
+        $location.path("/single/" + id);
+    }   
     
 });
 
-
+// $location.path("/single/" + id);
+//     
